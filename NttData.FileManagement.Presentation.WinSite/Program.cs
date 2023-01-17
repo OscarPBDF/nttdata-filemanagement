43f2ce7fd88dd7a286cbb064afb005f139c0ab33
operezdd <operezdd@emeal.nttdata.com>
2023-01-17 09:36:19 +0100

    Added project inicialization

---
+++ NttData.FileManagement.Presentation.WinSite/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NttData.FileManagement.Presentation.WinSite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
